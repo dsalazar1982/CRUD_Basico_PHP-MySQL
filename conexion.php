f9b73bc6a15dc0579d230797d617ded0e906278c
Daniel Salazar Erazo <dsalazar1982@yahoo.com>
2021-06-01 12:18:55 -0500

    Update

--- conexion.php
+++ conexion.php
@@ -5,12 +5,14 @@ $usuario = 'dba_user';
 $constraseña = 'dba_user@MariaDB2020*';
 
 try {
+    
     $pdo = new PDO ($link, $usuario, $constraseña);
-    echo "Conexion exitosa!";
+    
+    //echo "Conexion exitosa!";
 
-    foreach($pdo -> query('SELECT * FROM t_colores') as $filas){
+    /*foreach($pdo -> query('SELECT * FROM t_colores') as $filas){
         print_r($filas) . "<br/>";
-    }
+    }*/
     
 } catch (PDOException $e) {
     print "¡Error en conexion!: " . $e->getMessage() . "<br/>";
