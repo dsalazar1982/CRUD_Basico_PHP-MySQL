e57b539c258faecb51070b57e2edc0b45809eda9
Daniel Salazar Erazo <dsalazar1982@yahoo.com>
2021-06-01 11:37:35 -0500

    Primer confirmacion

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$link = 'mysql:host=10.0.0.50;dbname=db_colores';
+$usuario = 'user_dba';
+$constraseña = 'user_dba@MariaDB2020*';
+
+try {
+    $pdo = new PDO ($link, $usuario, $constraseña);
+    echo "Conexion exitosa!";
+    
+} catch (PDOException $e) {
+    print "¡Error en conexion!: " . $e->getMessage() . "<br/>";
+    die();    
+}
