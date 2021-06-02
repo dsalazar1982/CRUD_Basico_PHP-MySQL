d737d12ee6f9cceb83a4c3351cc61cfd4dac8185
Daniel Salazar Erazo <dsalazar1982@yahoo.com>
2021-06-02 15:32:34 -0500

    Update

--- index.php
+++ index.php
@@ -18,9 +18,7 @@ if ($_POST) {
     $sentencia_agregar->execute(array($color, $descripcion));
     header('Location: index.php');
 }
-
 ?>
-
 
 <!doctype html>
 <html lang="es">
@@ -29,60 +27,40 @@ if ($_POST) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/18130c8aad.js" crossorigin="anonymous"></script>
-
-
     <title>CRUD de Colores en PHP</title>
 </head>
 
 <body>
-
-
-
     <div class="container mt-5">
         <div class="row">
-
             <div class="col-md-6">
-
-                <?php foreach ($resultado as $dato): ?>
-                    <div
-                     class="alert alert-<?php echo $dato['color'] ?> text-uppercase"
-                      role="alert">
+                <?php foreach ($resultado as $dato) : ?>
+                    <div class="alert alert-<?php echo $dato['color'] ?> text-uppercase" role="alert">
                         <?php echo $dato['color'] ?> - <?php echo $dato['descripcion'] ?>
                         <a href="" class="float-right">
-                            <i class="far fa-edit"></i>
+                            <!-- <i class="far fa-edit"></i> -->
+                            <i class="fas fa-pencil-alt">
                         </a>
                     </div>
-                <?php endforeach?>
-
+                <?php endforeach ?>
             </div>
-
             <div class="col-md-6">
-                    <h2>AGREGAR ELEMENTO</h2>
-                    <form method="POST">
+                <h2>AGREGAR ELEMENTO</h2>
+                <form method="POST">
                     <input type="text" class="form-control mt-1" placeholder="Ingrese nombre del color" required name="color">
                     <input type="text" class="form-control mt-1" placeholder="Ingrese una descripcion el color" name="descripcion">
-                    <button class="btn btn-primary mt-3" >Agregar</button>
-                    </form>
+                    <button class="btn btn-primary mt-3">Agregar</button>
+                </form>
             </div>
-
-
         </div>
     </div>
 
-
-
-
-
-
     <!-- Optional JavaScript; choose one of the two! -->
-
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
